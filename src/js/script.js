8b35152cb77f1195cb8da6bb2b062c49304ec1af
VascThiago <thiago.vasconcellos01@fatecitapetininga.edu.br>
2022-12-16 15:54:47 -0300

    modificação do JS

--- src/js/script.js
+++ src/js/script.js
@@ -13,56 +13,19 @@ const handlePhone = (event) => {
   }
 
 // Validação dos campos
-function validar() {    
-    const nome = document.getElementById('nome');
-    const email = document.getElementById('email');
-    const telefone = document.getElementById('telefone');
-    const mensagem = document.getElementById('mensagem');
+function validar() {
+    const input = document.getElementsByName('input');
     const erro = document.getElementsByName('erro');
 
-    console.log(erro);
+    input.forEach(function(input, i) {
+        if(input.value == "") {
+            input.classList.remove('verde');
+            input.classList.add('vermelho');
+            erro[i].classList.add('ativa');
 
-    if(nome.value == "") {
-        nome.classList.remove('verde');
-        nome.classList.add('vermelho');
-        erro[0].classList.add('erro');
-        erro[0].innerHTML = "campo obrigatório";
-    } else {
-        nome.classList.add('verde');
-        erro[0].classList.remove('erro');
-        erro[0].innerHTML = "";
-    }
-    
-    if(email.value == "") {
-        email.classList.remove('verde');
-        email.classList.add('vermelho');
-        erro[1].classList.add('erro');
-        erro[1].innerHTML = "campo obrigatório";
-    } else {
-        email.classList.add('verde');
-        erro[1].classList.remove('erro');
-        erro[1].innerHTML = "";
-    }
-
-    if(telefone.value == "") {
-        telefone.classList.remove('verde');
-        telefone.classList.add('vermelho');
-        erro[2].classList.add('erro');
-        erro[2].innerHTML = "campo obrigatório";
-    } else {
-        telefone.classList.add('verde');
-        erro[2].classList.remove('erro');
-        erro[2].innerHTML = "";
-    }
-
-    if(mensagem.value == "") {
-        mensagem.classList.remove('verde');
-        mensagem.classList.add('vermelho');
-        erro[3].classList.add('errotextarea');
-        erro[3].innerHTML = "campo obrigatório";
-    } else {
-        mensagem.classList.add('verde');
-        erro[3].classList.remove('erro');
-        erro[3].innerHTML = "";
-    }
-}
+        } else {
+            input.classList.add('verde');
+            erro[i].classList.remove('ativa');
+        }
+    });
+};
